18f5d11d64205cc46cfe1b53676e79831e0ef72b
Leonard <leonard315@github.com>
2026-07-26 15:28:49 +0800

    fix: add storageBucket to Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:998583681939:web:e772d6d3bbcc09a623de66",
   "apiKey": "AIzaSyCf0msQ9R9Ylr00ohyRLAdVMuUS94Dzb70",
   "authDomain": "studio-4813687069-5fbf7.firebaseapp.com",
+  "storageBucket": "studio-4813687069-5fbf7.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "998583681939"
 };
