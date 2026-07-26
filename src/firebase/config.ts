42904306a7b79a97f00dc8cd625a80a29a934020
Leonard <leonard315@github.com>
2026-07-26 17:59:10 +0800

    fix: switch to balatasan-1 Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4813687069-5fbf7",
-  "appId": "1:998583681939:web:e772d6d3bbcc09a623de66",
-  "apiKey": "AIzaSyCf0msQ9R9Ylr00ohyRLAdVMuUS94Dzb70",
-  "authDomain": "studio-4813687069-5fbf7.firebaseapp.com",
-  "storageBucket": "studio-4813687069-5fbf7.firebasestorage.app",
-  "measurementId": "",
-  "messagingSenderId": "998583681939"
+  apiKey: "AIzaSyCdkF0moTUiUCVrYJCDl1qmRFKU2gr7hCk",
+  authDomain: "balatasan-1.firebaseapp.com",
+  projectId: "balatasan-1",
+  storageBucket: "balatasan-1.firebasestorage.app",
+  messagingSenderId: "970323798751",
+  appId: "1:970323798751:web:a6df5f317c961dcafd576a",
+  measurementId: "G-LE5QYZMXMC"
 };
